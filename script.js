2c449cc496a17867f753beba65a08edbb01c2bf8
KostyaStrakhov <konstantin.strakhov@gmail.com>
2022-12-03 18:44:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// let = 1user - не правильное имя переменной
+// let = const - не правильное имя переменной
+// let = return
+// let = user-Name
+// let = function
+
+
+let userName = prompt("Вкажіть ім'я");
+
+alert(`Привіт, ${userName}!`);
+
+let userBirthdayYear = prompt("Вкажіть рік народженя");
+
+let userAge = 2022 - userBirthdayYear;
+
+alert(`Вам ${userAge} років.`);
+
+let squareSide = prompt("Вкажіть довжину сторони квадрата")
+
+let squarePerimeter = squareSide * 4;
+
+alert(`Периметр квадрата = ${squarePerimeter}`);
+
+
+
+
